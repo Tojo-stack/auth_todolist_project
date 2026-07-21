45690722b35c052b77e1a1e7032cbfa02dbce7bd
Tojo-stack <toumandimbison@gmail.com>
2026-07-21 06:31:50 +0300

    fix: bind Express server to process.env.PORT

--- login-project/server.js
+++ login-project/server.js
@@ -11,7 +11,7 @@ const { password } = require('pg/lib/defaults');
 
 
 const app = express()
-const port = process.env.DB_PORT || 3000
+const port = process.env.PORT || 3000;
 
 
 // connexion à PostgreSQL
